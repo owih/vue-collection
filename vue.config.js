7a243e54afd4eee67bdf4024f43926eb25c649a3
Golovatenko D.F <golovatenko@intervolga.ru>
2022-09-12 00:18:48 +0300

    added vue public path on config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+// TODO: fix github pages redirect behavior
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-collection/'
+    : '/'
 })
